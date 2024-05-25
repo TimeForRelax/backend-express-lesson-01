9e59a13f82c8bef3884f5da8e4dfed79652b3877
Artem S <ars@anadeainc.com>
2024-05-25 20:02:43 +0300

    hotfix

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,3 @@ import { SETTINGS } from './settings';
 app.listen(SETTINGS.PORT, () => {
   console.log('...server started in port ' + SETTINGS.PORT);
 });
-// app.get(SETTINGS.PATH.VIDEOS, getVideosController)
-// app.use(SETTINGS.PATH.VIDEOS, videosRouter)
